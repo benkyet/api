f707f1e0295e859045c96d24462ca218330131de
James Nocentini <james.nocentini@gmail.com>
2013-08-15 14:37:58 +0100

    pushing to development branch

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 var config = require('config-env').define('NODE_ENV', function(config) {
     config.common({
-        name: 'benkyet',
-        express_port: process.env.PORT || 3000
+        name: 'benkyet'
     });
 
     config.config('dev', {
         mongo_uri: 'mongodb://localhost:27017/benkyet',
         mandrill_key: 'rkunpHmExBR5biUVEiwquQ',
-        mandrill_test: 'rR-vBeIRFJt5_Tesjflv1g'
+        mandrill_test: 'rR-vBeIRFJt5_Tesjflv1g',
+        port: 3003
     });
 
     config.config('prod', {
         mongo_uri: 'mongodb://localhost:27017/benkyet',
-        mandrill_key: ''
+        mandrill_key: '',
+        port: 3000
     });
 });
 

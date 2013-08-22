29b34ea2fa3593ea6aae1a78443b89923f74c7a3
James Nocentini <james.nocentini@gmail.com>
2013-08-22 12:00:26 +0100

    pushing to development branch

--- src/controllers/message-manager.js
+++ src/controllers/message-manager.js
@@ -11,7 +11,7 @@ function getId(id) {
 
 
 exports.inboundMessage = function(req, res) {
-    console.log(req.body.mandrill_events);
+    console.log(req.body.mandrill_events[0].msg);
     res.send('ok')
 }
 

a4db18ea3ecea41cddfac5217b24e44a61d49030
James Nocentini <james.nocentini@gmail.com>
2013-08-22 12:55:46 +0100

    pushing to development branch

--- src/controllers/message-manager.js
+++ src/controllers/message-manager.js
@@ -72,7 +72,7 @@ exports.inboundMessage = function(req, res) {
 //                                status: response_mandrill.status,
 //                                email: response_mandrill.email
 //                            };
-                            //console.log(m_err, m_res);
+                            console.log(m_err, m_res.text);
 
                         });
 

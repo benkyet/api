bc8934c2184874abb89e054ad2c3989183c617cc
James Nocentini <james.nocentini@gmail.com>
2013-08-30 15:06:08 +0100

    pushing to master branch

--- src/router.js
+++ src/router.js
@@ -35,7 +35,7 @@ module.exports = function(app) {
     app.del('/logout', AM.logout);
     app.post('/user', AM.createNewUser);
     app.delete('/user', auth, AM.deleteUser);
-    app.get('/user/items', auth, AM.userItems);
+    app.get('/user/items', auth, AM.userItems)
 
 
     app.get('/items', IM.getGroupList);

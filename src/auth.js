eebf1a419b4e5667117d11895ff030704452fa24
James Nocentini <james.nocentini@gmail.com>
2013-09-30 15:38:25 +0100

    logging accessToken

--- src/auth.js
+++ src/auth.js
@@ -60,6 +60,7 @@ module.exports.setup = function(app) {
         process.nextTick(function () {
             console.log(profile)
         //AM.createOrUpdateUserFromFB(profile);
+            console.log(accessToken);
             findOrCreate('facebook', profile, done);
         });
     }));
